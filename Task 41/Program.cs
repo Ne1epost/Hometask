7822cc9cd3350a66fded0c41dc0414b955e01045
Ne1epost <119726311+Ne1epost@users.noreply.github.com>
2023-01-25 22:13:35 +0300

    Задача 40

---
+++ Task 41/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Write("Введите любое количество чисел от -1000 до 1000 ЧЕРЕЗ ПРОБЕЛ: ");
+    int[] newarray = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int count = 0;
+for (int i = 0; i < newarray.Length; i++)
+{
+    if (newarray[i] > 0)
+    {
+        count++;
+  }
+
+}
+    Console.WriteLine($"Введено чисел больше 0: {count}");
+
